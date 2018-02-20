9822a665dea78651f0835e7f9fe4f81a783249c6
justy97 <33075784+justy97@users.noreply.github.com>
2018-02-19 21:40:01 -0800

    Update problemC.R

--- hwk3/problemC.R
+++ hwk3/problemC.R
@@ -8,31 +8,13 @@ boardgame <- function(nreps,inipos){
         if (pos > 7){
             pos <- pos - 8 
             nwins <- nwins + 1
-            winning <- pos + 1
-        }
-    }
-    print(nreps/nwins)
-    print(winning/nreps)
-}
-boardgame(100000,0)
-
-boardgame <- function(nreps,inipos){
-    nwins <- 0
-    pos <- inipos
-    winning <- 0
-    for (rep in 1:nreps){
-        roll <- sample(1:6,1)
-        pos <- pos + roll
-        if (pos > 7){
-            pos <- pos - 8 
-            nwins <- nwins + 1
             winning <- winning + pos + 1
         }
     }
     print(nreps/nwins)
     print(winning/nreps)
 }
-boardgame(100000,0)
+boardgame(20000,0)
 
 
 boardexpected <- function(nreps,inipos){
@@ -53,11 +35,11 @@ boardexpected <- function(nreps,inipos){
     }
     print(mean(wins))
 }
-boardexpected(100000,0)
-boardexpected(100000,1)
-boardexpected(100000,2)
-boardexpected(100000,3)
-boardexpected(100000,4)
-boardexpected(100000,5)
-boardexpected(100000,6)
-boardexpected(100000,7)
+boardexpected(20000,0)
+boardexpected(20000,1)
+boardexpected(20000,2)
+boardexpected(20000,3)
+boardexpected(20000,4)
+boardexpected(20000,5)
+boardexpected(20000,6)
+boardexpected(20000,7)
