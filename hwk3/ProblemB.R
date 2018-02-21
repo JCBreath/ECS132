adc664326884a49d16b76ec1932cd2205883445d
justy97 <33075784+justy97@users.noreply.github.com>
2018-02-20 19:51:57 -0800

    Create ProblemB.R

---
+++ hwk3/ProblemB.R
@@ -0,0 +1,41 @@
+#skewness = E[(N - EN)^3 / stdvN] 
+#r = 3, p = 0.4
+
+
+
+skew <- function(nreps,r,p){
+    
+    Result_V <- vector(length = nreps)
+    SK_V <- vector(length = nreps)
+    Var_V <- vector(length = nreps)
+    
+    #simulate the game, and put results in Result_V
+    for (rep in 1:nreps){
+        total <- 0
+        number <- 0
+        while (total < r){
+            get = sample(0:1,prob = c((1-p),p))
+            total = total + get
+            number = number + 1
+        }
+        Result_V[rep] = number
+    }
+    
+    EN <- mean(Result_V)
+        
+    
+    for (rep_1 in 1:nreps){
+        Var_V[rep_1] = (Result_V[rep_1] - EN)^2
+    }
+    Var <- mean(Var_V)           
+    stdV <- sqrt(Var)
+    
+    for (rep_2 in 1:nreps){
+        SK_V[rep_2] = ((Result_V[rep_2] - EN)^3 /stdV)
+    }
+    
+    result <- mean(SK_V)
+    print(result)
+}
+
+skew(50000,3,0.4)
