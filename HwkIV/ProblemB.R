e99fa4ff453bfeed125a8938928a29ac7cd59b46
JCBreath <Mimafama0@qq.com>
2018-02-28 16:38:48 -0800

    Add files via upload

--- HwkIV/ProblemB.R
+++ HwkIV/ProblemB.R
@@ -1,44 +1,9 @@
-#skewness = E[(N - EN)^3 / stdvN^3] 
-#r = 3, p = 0.4
-
-
-
-skew <- function(nreps,r,p){
-    
-    Result_V <- vector(length = nreps)
-    SK_V <- vector(length = nreps)
-    Var_V <- vector(length = nreps)
-    
-    #simulate the game, and put results in Result_V
-    for (rep in 1:nreps){
-        total <- 0
-        number <- 0
-        while (total < r){
-            get = sample(0:1,size=1,prob = c((1-p),p),replace = FALSE)
-            total = total + get
-            number = number + 1
-        }
-        Result_V[rep] = number
-    }
-    
-    EN <- mean(Result_V)
-    #print(EN)
-    
-    for (rep_1 in 1:nreps){
-        Var_V[rep_1] = (Result_V[rep_1] - EN)^2
-    }
-    Var <- mean(Var_V)           
-    stdV <- sqrt(Var)
-    
-    #print(Var)
-    
-    stdV<- sqrt(Var)
-    for (rep_2 in 1:nreps){
-        SK_V[rep_2] = ((Result_V[rep_2] - EN)^3 /(stdV^3))
-    }
-    
-    result <- mean(SK_V)
-    print(result)
+simProbB <- function(nreps,lambda,alpha,beta) {
+	E_D <- mean(rpois(nreps, lambda)*rbeta(nreps,alpha,beta,ncp=0))
+	E_D_2 <- mean(rpois(nreps, lambda)^2*rbeta(nreps,alpha,beta,ncp=0))
+	Var_D <- E_D_2 - E_D^2
+	cat("Mean:",E_D,'\n')
+	cat("Variance:",Var_D,'\n')
 }
 
-skew(40000,3,0.4)
+#simProbB(10000,1,0.2,0.2)
