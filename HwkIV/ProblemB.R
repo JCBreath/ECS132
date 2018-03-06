d020a7a2e85a3d6cdcba55e1f9143d72a879c8f0
JCBreath <Mimafama0@qq.com>
2018-03-05 23:36:03 -0800

    Add files via upload

--- HwkIV/ProblemB.R
+++ HwkIV/ProblemB.R
@@ -1,15 +1,9 @@
 simProbB <- function(nreps,lambda,alpha,beta) {
-	nums <- rpois(nreps,lambda)
-	defrat <- rbeta(nreps,alpha,beta,ncp=0)
-	defNum <- sum(runif(nums[1])<defrat[1])
-	for (i in 2:nreps){
-		s <- sum (runif(nums[i])<defrat[i])
-		defNum <- c(defNum,s)
-	}	
-	mean = mean(defNum)
-	var = sum((defNum-mean)^2)/nreps
-	cat("Mean:",mean,'\n')
-	cat("Variance:",var,'\n')
-}	
-simProbB(50000,1,0.2,0.2)
-	
+	N <- rpois(nreps, lambda)
+	p <- rbeta(nreps,alpha,beta,ncp=0)
+	E_D <- mean(N*p)
+	Var_D <- mean((N*p-E_D)^2)
+	cat("Mean:",E_D,'\n')
+	cat("Variance:",Var_D,'\n')
+}
+simProbB(1000000,1,0.2,0.2)
