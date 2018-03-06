16945839a017112d967ad05a94533a85bae8fb96
justy97 <33075784+justy97@users.noreply.github.com>
2018-03-05 21:52:44 -0800

    Update ProblemB.R

--- HwkIV/ProblemB.R
+++ HwkIV/ProblemB.R
@@ -1,9 +1,15 @@
 simProbB <- function(nreps,lambda,alpha,beta) {
-	E_D <- mean(rpois(nreps, lambda)*rbeta(nreps,alpha,beta,ncp=0))
-	E_D_2 <- mean(rpois(nreps, lambda)^2*rbeta(nreps,alpha,beta,ncp=0))
-	Var_D <- E_D_2 - E_D^2
-	cat("Mean:",E_D,'\n')
-	cat("Variance:",Var_D,'\n')
-}
-
-#simProbB(10000,1,0.2,0.2)
+	nums <- rpois(nreps,lambda)
+	defrat <- rbeta(nreps,alpha,beta,ncp=0)
+	defNum <- sum(runif(nums[1])<defrat[1])
+	for (i in 2:nreps){
+		s <- sum (runif(nums[i])<defrat[i])
+		defNum <- c(defNum,s)
+	}	
+	mean = mean(defNum)
+	var = sum((defNum-mean)^2)/nreps
+	cat("Mean:",mean,'\n')
+	cat("Variance:",var,'\n')
+}	
+simProbB(50000,1,0.2,0.2)
+	
